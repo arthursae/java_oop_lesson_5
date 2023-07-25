27468e08c763eab5f42e59b2a67e1975e07bc14c
arthursae <118846805+arthursae@users.noreply.github.com>
2023-07-25 21:26:15 +0500

    Create UserRepository.java

---
+++ repositories/UserRepository.java
@@ -0,0 +1,15 @@
+package lesson4.repositories;
+
+import lesson4.models.User;
+
+import java.util.List;
+
+public interface UserRepository<T extends User> {
+    void create(T user);
+
+    List<T> getAll();
+
+    int remove(String fullName);
+
+    List<T> getAllByGroupTitle(String groupTitle);
+}
