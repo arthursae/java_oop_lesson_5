c4b21188f6748e7d0eb4535106a408801907cc76
arthursae <118846805+arthursae@users.noreply.github.com>
2023-07-25 21:24:02 +0500

    Add files via upload

---
+++ controllers/TeacherController.java
@@ -0,0 +1,43 @@
+package lesson4.controllers;
+
+import lesson4.models.Teacher;
+import lesson4.services.UserService;
+
+import java.util.List;
+
+public class TeacherController implements UserController<Teacher> {
+
+    private final UserService<Teacher> userService;
+
+    public TeacherController(UserService<Teacher> userService) {
+        this.userService = userService;
+    }
+
+    //@PostMethod("/teachers")
+    @Override
+    public void create(String fullName, Integer age, String phoneNumber, String groupTitle) {
+        userService.create(fullName, age, phoneNumber, groupTitle);
+    }
+
+    //@GetMethod("/teachers")
+    @Override
+    public List<Teacher> getAll() {
+        return userService.getAll();
+    }
+
+    //@DeleteMethod("/teachers")
+    @Override
+    public int remove(String fullName) {
+        return userService.remove(fullName);
+    }
+
+    @Override
+    public List<Teacher> getAllSortByFullName() {
+        return userService.getAllSortByFullName();
+    }
+
+    @Override
+    public List<Teacher> getAllSortById() {
+        return userService.getAllSortById();
+    }
+}
