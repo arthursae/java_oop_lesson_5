8e08140af3fe988639d6e7f75722c94fc58e136d
arthursae <118846805+arthursae@users.noreply.github.com>
2023-07-26 02:35:24 +0500

    Update UserView.java

--- view/UserView.java
+++ view/UserView.java
@@ -2,8 +2,12 @@ package lesson4.view;
 
 public interface UserView {
     void sendOnConsole(); //GET
+
     void sendOnConsole(SortType sortType); //GET
+
     void create(String fullName, Integer age, String phoneNumber, String groupTitle);//POST
+
     int removeUser(String fullName); //DELETE
 
+    void updateUserGroupById(int id, String groupTitle); //UPDATE
 }
