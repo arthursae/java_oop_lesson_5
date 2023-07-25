770473e15317b791694ab7b3305ddc9be6a2d650
arthursae <118846805+arthursae@users.noreply.github.com>
2023-07-26 02:38:40 +0500

    Update UserRepository.java

--- repositories/UserRepository.java
+++ repositories/UserRepository.java
@@ -7,6 +7,8 @@ import java.util.List;
 public interface UserRepository<T extends User> {
     void create(T user);
 
+    void updateGroupTitleById(int id, String groupTitle);
+
     List<T> getAll();
 
     int remove(String fullName);
