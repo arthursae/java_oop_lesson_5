de63f56fce5801b1fd3e58dc64fb9261554a066c
arthursae <118846805+arthursae@users.noreply.github.com>
2023-07-25 23:25:02 +0500

    Create UniversityApp.java

---
+++ UniversityApp.java
@@ -0,0 +1,150 @@
+package lesson4;
+
+import lesson4.controllers.GroupController;
+import lesson4.controllers.StudentController;
+import lesson4.controllers.TeacherController;
+import lesson4.repositories.StudentRepository;
+import lesson4.repositories.TeacherRepository;
+import lesson4.services.GroupService;
+import lesson4.services.StudentService;
+import lesson4.services.TeacherService;
+import lesson4.view.GroupView;
+import lesson4.view.StudentView;
+import lesson4.view.TeacherView;
+import lesson4.view.SortType;
+
+import java.util.Scanner;
+
+public class UniversityApp {
+
+    private static StudentRepository studentRepository;
+    private static TeacherRepository teacherRepository;
+    private static StudentService studentService;
+    private static TeacherService teacherService;
+    private static StudentController studentController;
+    private static TeacherController teacherController;
+
+    public static void main(String[] args) {
+        StudentView student = getStudentController();
+        TeacherView teacher = getTeacherController();
+        GroupView groupView = getGroupView();
+//      -------------------------------------------------------------------------
+
+        student.create("Ivan Morozov", 18, "02", "11Б");
+        student.create("Ivan Morozov", 18, "02", "11Б");
+        student.create("Petr Vorobev", 19, "03", "10А");
+        student.create("Sidor Sidorov", 20, "112", "10А");
+        student.create("Elena Ivanova", 19, "911", "10А");
+        student.create("Anna Morozova", 17, "01", "11А");
+        teacher.create("Konstantin Sidorov", 42, "108", "11А");
+        teacher.create("Maria Pavlova", 33, "234", "10А");
+
+        //groupView.printAllFromGroup("11А");
+        //groupView.printAllFromGroup("10А");
+        //student.sendOnConsole();
+        //student.sendOnConsole(SortType.NAME);
+        //student.sendOnConsole(SortType.ID);
+
+        //student.removeUser("Ivan Morozov");
+
+        //student.sendOnConsole(SortType.NAME);
+        //teacher.sendOnConsole(SortType.NAME);
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("Введите запрос: ");
+            String input = scanner.nextLine().trim();
+
+            if ("exit".equalsIgnoreCase(input)) break;
+
+            if (!input.isEmpty()) {
+                String[] commands = input.split("\\s+");
+                int commandslength = commands.length;
+
+                if ("/get-student".equalsIgnoreCase(commands[0])) {
+
+                    if (commandslength == 2) {
+                        if ("name".equalsIgnoreCase(commands[1])) {
+                            student.sendOnConsole(SortType.NAME);
+                        } else if ("id".equalsIgnoreCase(commands[1])) {
+                            student.sendOnConsole(SortType.ID);
+                        } else {
+                            System.out.println("Неправильный параметр сортировки: " + commands[1]);
+                        }
+                    } else {
+                        student.sendOnConsole(SortType.NONE);
+                    }
+                } else if ("/get-group".equalsIgnoreCase(commands[0])) {
+
+                    if (commandslength == 2) {
+                        groupView.printAllFromGroup(commands[1]);
+                    } else {
+                        System.out.println("Не указано название группы");
+                    }
+                } else if ("/create-student".equalsIgnoreCase(commands[0])) {
+
+                    if (commandslength == 5) {
+                        String studentName = commands[1];
+                        Integer studentAge = null;
+                        String studentPhone = commands[3];
+                        String studentGroup = commands[4];
+
+                        try {
+                            studentAge = Integer.parseInt(commands[2]);
+                        } catch (NumberFormatException ex) {
+                            System.out.println("Указан неправильный возраст студентв");
+                        }
+
+                        if (studentAge != null) {
+                            student.create(studentName.replace("_", " "), studentAge, studentPhone, studentGroup);
+                            System.out.println("Студент '" + studentName + "' добавлен");
+                        } else {
+                            System.out.println("Не удалось добавить студента");
+                        }
+                    } else {
+                        System.out.println("Не указаны все данные студента: имя_фамилия возраст телефон группа");
+                    }
+
+                } else if ("/delete-teacher".equalsIgnoreCase(commands[0])) {
+                    if (commandslength == 2) {
+                        String teacherName = commands[1];
+                        teacher.removeUser(teacherName.replace("_", " "));
+                    } else {
+                        System.out.println("Не указано имя учителя");
+                    }
+
+                } else if ("/set-group-student-by-id".equalsIgnoreCase(commands[0])) {
+                    if (commandslength == 3) {
+                        // что должна делать эта команда не понятно...
+                    } else {
+                        System.out.println("Не указаны все данные: айди и название группы");
+                    }
+                } else {
+                    System.out.println("Неправильная комманда");
+                }
+            }
+        }
+    }
+
+    private static StudentView getStudentController() {
+        studentRepository = new StudentRepository();
+        studentService = new StudentService(studentRepository);
+        studentController = new StudentController(studentService);
+        return new StudentView(studentController);
+    }
+
+    private static TeacherView getTeacherController() {
+        teacherRepository = new TeacherRepository();
+        teacherService = new TeacherService(teacherRepository);
+        teacherController = new TeacherController(teacherService);
+        return new TeacherView(teacherController);
+    }
+
+    private static GroupView getGroupView() {
+        GroupService groupService = new GroupService(studentService, teacherService);
+        GroupController groupController = new GroupController(groupService);
+        return new GroupView(groupController);
+    }
+}
